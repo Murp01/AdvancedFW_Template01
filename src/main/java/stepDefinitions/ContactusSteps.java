5b2dd0a03d99bd88c2e21cf9e8e9320184a5d3d8
Murp01 <pmurphy35@hotmail.com>
2018-12-26 11:49:47 +0000

    Update ChromeDriver and added code to enter test in contact us form
    Original Chromedriver was not compatible with the version of Chrome so
    updated it.  Also code has been added to run steps where text is added
    into all fields on form.

--- src/main/java/stepDefinitions/ContactusSteps.java
+++ src/main/java/stepDefinitions/ContactusSteps.java
@@ -2,13 +2,16 @@ package stepDefinitions;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -26,32 +29,37 @@ public class ContactusSteps {
 		this.driver.manage().timeouts().pageLoadTimeout(123, TimeUnit.SECONDS);
 	}
 	
+	@After
+	public void teardown() throws InterruptedException{
+		Thread.sleep(4000);
+		this.driver.manage().deleteAllCookies();
+		this.driver.quit();
+	}
+	
 	
 	@Given("^I access webdriveruniversity contact us form$")
 	public void i_access_webdriveruniversity_contact_us_form() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		driver.get("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+		
 	}
 
 	@When("^I enter a valid firstname$")
 	public void i_enter_a_valid_firstname() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector("input[name='first_name']")).sendKeys("Paul");
 	}
 
 	@When("^I enter a valid lastname$")
-	public void i_enter_a_valid_lastname(DataTable arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    // For automatic transformation, change DataTable to one of
-	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
-	    throw new PendingException();
+	public void i_enter_a_valid_lastname(DataTable dataTable) throws Throwable {
+		List<List<String>> data = dataTable.raw();
+		
+		driver.findElement(By.cssSelector("input[name='last_name']")).sendKeys(data.get(0).get(1));
+		
 	}
 
 	@When("^I enter a valid email address$")
 	public void i_enter_a_valid_email_address() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		driver.findElement(By.cssSelector("input[name='email']")).sendKeys("webdriveruniversity.com");
 	}
 
 	@When("^I enter comments$")
