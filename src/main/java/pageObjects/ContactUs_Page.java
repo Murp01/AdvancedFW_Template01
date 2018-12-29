6adaa744e0b3fcca44d82836f4aa23dfe47ec785
pmurp <pmurp@YogaFire-LP>
2018-12-29 02:46:37 +0000

    Replaced ContactUs SD code with page object methods.  Error with assert
    This is related to the assertion not displaying the correct string
    compared with original - check regex or expected string?

--- src/main/java/pageObjects/ContactUs_Page.java
+++ src/main/java/pageObjects/ContactUs_Page.java
@@ -2,17 +2,17 @@ package pageObjects;
 
 import java.io.IOException;
 import java.util.List;
-
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import cucumber.api.DataTable;
 
 public class ContactUs_Page extends BasePage {
 	public @FindBy(xpath = "//input[@name='first_name']") WebElement textField_FirstName;
 	public @FindBy(xpath = "//input[@name='last_name']") WebElement textField_LastName;
 	public @FindBy(xpath = "//input[@name='email']") WebElement textField_EmailAddress;
-	public @FindBy(xpath = "//input[@name='message']") WebElement textField_Message;
+	public @FindBy(xpath = "//textarea[@name='message']") WebElement textField_Message;
 	public @FindBy(xpath = "//input[@value='SUBMIT']") WebElement button_Submit;
 
 	public ContactUs_Page() throws IOException {
@@ -48,6 +48,13 @@ public class ContactUs_Page extends BasePage {
 	
 	public ContactUs_Page clickOnSubmitButton() throws Exception {
 		waitAndClickElement(button_Submit);
+		return new ContactUs_Page();
+	}
+	
+	public ContactUs_Page confirmContactFormSubmissionWasSuccessful() throws IOException {
+		WebElement thanksContactUsPage = getDriver().findElement(By.xpath("//div[@id='contact_reply']"));
+		WaitUntilWebElementIsVisible(thanksContactUsPage);
+		Assert.assertEquals("thankyouforyourmessage", thanksContactUsPage.getText().toLowerCase().replaceAll("[ ()0-9]", ""));
 		return new ContactUs_Page();
 	}
 
--- src/main/java/stepDefinitions/ContactusSteps.java
+++ src/main/java/stepDefinitions/ContactusSteps.java
@@ -14,49 +14,37 @@ public class ContactusSteps extends DriverFactory{
 	
 	@Given("^I access webdriveruniversity contact us form$")
 	public void i_access_webdriveruniversity_contact_us_form() throws Throwable {
-		getDriver().get("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
-		
+		contactUsPage.getContactUsPage();	
 	}
 
 	@When("^I enter a valid firstname$")
-	public void i_enter_a_valid_firstname() throws Throwable {
-		Thread.sleep(3000);
-		getDriver().findElement(By.cssSelector("input[name='first_name']")).sendKeys("Paul");
+	public void i_enter_a_valid_firstname() throws Exception {
+		contactUsPage.enterFirstName();
 	}
 
 	@When("^I enter a valid lastname$")
-	public void i_enter_a_valid_lastname(DataTable dataTable) throws Throwable {
-		List<List<String>> data = dataTable.raw();
-		
-		getDriver().findElement(By.cssSelector("input[name='last_name']")).sendKeys(data.get(0).get(1));
-		
+	public void i_enter_a_valid_lastname(DataTable dataTable) throws Exception {
+		contactUsPage.enterLastname(dataTable, 0, 1);	
 	}
 
 	@When("^I enter a valid email address$")
-	public void i_enter_a_valid_email_address() throws Throwable {
-		getDriver().findElement(By.cssSelector("input[name='email']")).sendKeys("webdriveruniversity@hotmail.com");
+	public void i_enter_a_valid_email_address() throws Exception {
+		contactUsPage.enterEmailAddress("webdriveruniversity@hotmail.com");
 	}
 
 	@When("^I enter comments$")
-	public void i_enter_comments(DataTable dataTable) throws Throwable {
-		List<List<String>> data = dataTable.raw(); 
-		getDriver().findElement(By.xpath("//textarea[@placeholder='Comments']")).sendKeys(data.get(0).get(0));
-		getDriver().findElement(By.xpath("//textarea[@placeholder='Comments']")).sendKeys(data.get(0).get(1));
+	public void i_enter_comments(DataTable dataTable) throws Exception {
+		contactUsPage.enterMessage(dataTable, 0, 1);
 	}
 
 	@When("^I click on the submit button$")
-	public void i_click_on_the_submit_button() throws Throwable {
-		getDriver().findElement(By.cssSelector("input[value='SUBMIT']")).click();
+	public void i_click_on_the_submit_button() throws Exception {
+		contactUsPage.clickOnSubmitButton();
 	}
 
 	@Then("^the information will successfully be submitted via the contact us form$")
-	public void the_information_will_successfully_be_submitted_via_the_contact_us_form() throws Throwable {
-		WebElement thanksContactPage = getDriver().findElement(By.xpath("//div[@id='contact_reply']"));		
-		Thread.sleep(3000);
-		//value after replace all is a regex value, removes spaces or digits with "" - no space
-		Assert.assertEquals("thankyouforyourmessage!", thanksContactPage.getText().toLowerCase()
-				.replaceAll("[ ()0-9]", ""));
-		System.out.println(thanksContactPage);
+	public void the_information_will_successfully_be_submitted_via_the_contact_us_form() throws Exception {
+		contactUsPage.confirmContactFormSubmissionWasSuccessful();
 	}
 	
 
