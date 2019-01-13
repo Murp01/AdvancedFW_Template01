aa1ced2e18b64a476c58260f3f6791f8a2f8e43c
Murp01 <pmurphy35@hotmail.com>
2019-01-13 20:09:44 +0000

    Added chromedriver to POM and attempting to change constant
    POM creates webdriver jars rather than .exe.  Also, the file path will
    need to be changed so that if uopdated we don't need to edit the
    filepath for new version folder names.

--- src/main/java/utils/Constant.java
+++ src/main/java/utils/Constant.java
@@ -5,7 +5,7 @@ public class Constant {
 	public final static String CONFIG_PROPERTIES_DIRECTORY = "properties\\config.properties";
 	
 	public final static String GECKO_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\resources\\other\\geckodriver.exe";	
-	public final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\resources\\other\\chromedriver.exe";	
+	public final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "/../..\\.m2\\repository\\org\\seleniumhq\\selenium\\selenium-chrome-driver\\3.141.59\\chromedriver.exe";	
 	public final static String IE_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\resources\\other\\IEDriverServer.exe";
 
 
